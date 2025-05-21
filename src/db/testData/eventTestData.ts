b88081404ef02de358a4cc57443111e72abb67f5
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-05-21 11:42:25 +0100

    event test data

---
+++ src/db/testData/eventTestData.ts
@@ -0,0 +1,29 @@
+import { EventInput } from '../../types/Event';
+
+export const eventTestData: EventInput[] = [
+  {
+    title: 'Labour Contract Conclusion Party',
+    description: "We're off this planet!",
+    location: 'Mining Station Aratake',
+    price: 0.0,
+    start_time: '2025-07-01T09:00:00Z',
+    end_time: '2025-07-01T17:00:00Z',
+  },
+  {
+    title: 'Bonding and You: A Planetary Survey Workshop',
+    description:
+      "Learn from The Company's experts about how to craft the perfect pitch and obtain insurance.",
+    location: 'Port FreeCommerce',
+    price: 100.0,
+    start_time: '2025-08-15T10:00:00Z',
+    end_time: '2025-08-15T18:00:00Z',
+  },
+  {
+    title: 'Planetary Survey',
+    description: "We'll be collecting a range of soil samples for processing.",
+    location: 'Survey Site OQ17Z4Y',
+    price: 5.0,
+    start_time: '2025-09-10T11:00:00Z',
+    end_time: '2025-09-10T16:00:00Z',
+  },
+];
