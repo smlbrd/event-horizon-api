3bf86d944961392e433ec2c9b4371effe70e5aee
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-05-23 17:22:00 +0100

    updated types

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -2,9 +2,3 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: { username: string; role: string };
-  }
-}
