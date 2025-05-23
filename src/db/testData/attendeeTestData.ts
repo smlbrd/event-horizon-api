4bbaa80cf6fdfeb39fa603be2bebb4995cc3382a
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-05-23 15:10:12 +0100

    add attendee test data

---
+++ src/db/testData/attendeeTestData.ts
@@ -0,0 +1,9 @@
+export const attendeeTestData = [
+  { user_id: 1, event_id: 1, status: 'attending' },
+  { user_id: 1, event_id: 2, status: 'attending' },
+
+  { user_id: 2, event_id: 2, status: 'cancelled' },
+  { user_id: 2, event_id: 3, status: 'attending' },
+
+  { user_id: 3, event_id: 1, status: 'cancelled' },
+];
