e897bbfa9d85e6a6b96a0da40f3e2982fc3d9e3b
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-05-23 17:22:40 +0100

    add devData and run-seed function

---
+++ src/db/devData/attendeeDevData.ts
@@ -0,0 +1,19 @@
+export const attendeeDevData = [
+  { user_id: 1, event_id: 2, status: 'attending' },
+  { user_id: 1, event_id: 3, status: 'attending' },
+  { user_id: 1, event_id: 5, status: 'attending' },
+
+  { user_id: 2, event_id: 2, status: 'cancelled' },
+  { user_id: 2, event_id: 3, status: 'attending' },
+  { user_id: 2, event_id: 7, status: 'attending' },
+
+  { user_id: 3, event_id: 1, status: 'cancelled' },
+  { user_id: 3, event_id: 4, status: 'attending' },
+  { user_id: 3, event_id: 10, status: 'attending' },
+
+  { user_id: 4, event_id: 5, status: 'attending' },
+  { user_id: 4, event_id: 6, status: 'attending' },
+
+  { user_id: 5, event_id: 8, status: 'cancelled' },
+  { user_id: 5, event_id: 9, status: 'attending' },
+];
