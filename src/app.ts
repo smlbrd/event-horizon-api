c0a4858c6587096ec176a17b239b9b49dafe372a
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-05-24 13:49:03 +0100

    error handling and shutdown for pool connections

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ import {
   notFoundErrorHandler,
 } from './middleware/errorHandler';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', appRoutes);
